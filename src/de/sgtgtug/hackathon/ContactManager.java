00fc61aea18f071c04a9f94483fe25536f8450f4
Sascha Selzer <selzer@namelessApple1.local>
2010-09-04 15:57:14 +0200

    Add settings to Option Menu of HelpMe Activity.

--- src/de/sgtgtug/hackathon/ContactManager.java
+++ src/de/sgtgtug/hackathon/ContactManager.java
@@ -17,14 +17,11 @@
 package de.sgtgtug.hackathon;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
--- src/de/sgtgtug/hackathon/HelpME.java
+++ src/de/sgtgtug/hackathon/HelpME.java
@@ -1,13 +1,32 @@
 package de.sgtgtug.hackathon;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class HelpME extends Activity {
-    /** Called when the activity is first created. */
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	menu.add(0, Menu.FIRST, Menu.NONE, "Settings");
+    	return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch (item.getItemId()){
+    	case Menu.FIRST: 
+    		startActivity(new Intent(this, HelpMePreferences.class));
+    		break;
+    	}
+    	return super.onOptionsItemSelected(item);
     }
 }
